429324d9f534af8a9ca921afe461e70426dfd443
carlosscarranza <carlosscarranza@yahoo.com>
2021-09-06 18:56:38 -0500

    mysql

--- CRUD_Java_MVC/src/java/Config/Conexion.java
+++ CRUD_Java_MVC/src/java/Config/Conexion.java
@@ -5,10 +5,28 @@
  */
 package Config;
 
+import java.sql.*;
+
 /**
  *
  * @author axter
  */
 public class Conexion {
+ 
+    Connection con;
+    
+    public Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://sql10.freesqldatabase.com:3306/sql10435141", "sql10435141", "XsiJKDuXeg");
+        }
+        catch (Exception ex){
+            System.err.println("error" + ex);
+        }
+    }
+    
+    public Connection getConnection(){
+        return con;
+    }
     
 }
