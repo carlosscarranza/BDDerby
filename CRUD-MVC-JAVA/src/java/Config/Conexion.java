b0d3b287db5ab2a670c041591905ee54180ee249
carlosscarranza <carlosscarranza@yahoo.com>
2021-09-15 20:43:39 -0500

    add

--- CRUD-MVC-JAVA/src/java/Config/Conexion.java
+++ CRUD-MVC-JAVA/src/java/Config/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
     public Conexion(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://sql10.freesqldatabase.com:3306/sql10435141","sql10435141","XsiJKDuXeg");            
+            con=DriverManager.getConnection("jdbc:mysql://sql10.freesqldatabase.com:3306/sql10437558","sql10437558","zbIRcy5uzd");            
         } catch (Exception e) {
             System.err.println("Error"+e);
         }
